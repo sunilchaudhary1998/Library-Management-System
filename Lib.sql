5255c8160be6290cd246f1656225e39460eaa4f9
SUNIL JAYRAM CHAUDHARY <107506936+sunilchaudhary1998@users.noreply.github.com>
2023-08-31 10:56:45 +0530

    Add files via upload
    This Data base table to store data

---
+++ Lib.sql
@@ -0,0 +1,42 @@
+
+
+---for login user data
+---info Table
+CREATE TABLE [info](
+	[Name] [varchar](30),
+	[Number] [bigint],
+	[Email] [varchar](30),
+	[Password] [varchar](30)
+)
+
+----------------------------------------------------------------------------------------------------------
+
+---ALL books data will store here from APi
+---All_books
+CREATE TABLE [All_books](
+	[Book_id] [int] IDENTITY(1,1),
+	[title] [varchar](100),
+	[authors] [varchar](500),
+	[average_rating] [decimal](10, 2),
+	[isbn] [varchar](500),
+	[isbn13] [varchar](500),
+	[language_code] [varchar](50),
+	[num_pages] [int],
+	[ratings_count] [int],
+	[text_reviews_count] [int],
+	[publication_date] [date],
+	[publisher] [varchar](100),
+	[is_deleted] [int],
+	[is_issued] [int] 
+) 
+
+--------------------------------------------------------------------------------------------------------------------
+---ALL member who took books their details saved here
+---Member
+CREATE TABLE [Member](
+	[Member_Id] [int] IDENTITY(1,1),
+	[Member_Name] [varchar](100),
+	[Book_id] [int],
+	[Fees] [decimal](10, 2)
+	)
+----------------------------------------------------------------------------------------------------------------
